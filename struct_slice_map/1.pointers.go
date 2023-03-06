924a5575c9f8649a4367fad3ce1ecd924f52d61a
Oh Dowon <dhehdnjs03@gmail.com>
2023-03-06 23:30:42 +0900

    pointer의 zero value는 nil

--- struct_slice_map/1.pointers.go
+++ struct_slice_map/1.pointers.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	i, j := 42, 2701
 
-	var p *int = &i // i 주소 저장
+	var p *int = &i // i 주소 저장, 포인터의 zero value는 nil
 	fmt.Println(*p) // 역참조(간접참조)로 i 값 읽기
 	*p = 21         // 역참조로 i 값 변경
 	fmt.Println(i)
