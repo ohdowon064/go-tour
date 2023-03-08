3d3a2581369af2ad1a19a3d736b0193f6e5853a7
Oh Dowon <dhehdnjs03@gmail.com>
2023-03-08 21:39:44 +0900

    channel

--- basic/15.numeric-constants.go
+++ basic/15.numeric-constants.go
@@ -5,7 +5,7 @@ import "fmt"
 const (
 	// Create a huge number by shifting a 1 bit left 100 places.
 	// In other words, the binary number that is 1 followed by 100 zeroes.
-	Big = 1 << 100
+	Big = 1 << 63
 	// Shift it right again 99 places, so we end up with 1<<1, or 2.
 	Small = Big >> 99
 )
@@ -18,5 +18,5 @@ func needFloat(x float64) float64 {
 func main() {
 	fmt.Println(needInt(Small))
 	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	fmt.Printf("%d\n", Big)
 }
---
+++ concurrency/2.channels.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// channel은 사용되기 전에 make로 생성되어야한다.
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // channel c에 sum을 보낸다.
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	x, y := <-c, <-c // channel c로부터 값을 받는다.
+
+	fmt.Println(x, y, x+y)
+}
